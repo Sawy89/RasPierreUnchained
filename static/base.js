5545c015acd7a7bc09652b040f52ee683aef50b6
Sawy89 <ddeennyy@hotmail.it>
2020-03-27 18:02:01 +0100

    sidebar style

--- static/base.js
+++ static/base.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#sidebarCollapse').addEventListener('click', function() {
         document.querySelector('#sidebar').classList.toggle('active');
+        document.querySelector('#sidebarCollapse').classList.toggle('active');
     });
 
 });
