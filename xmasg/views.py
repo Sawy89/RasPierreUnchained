0fef8bbe933aeeebbb6910cdbfc9c63fe4ce2708
Sawy89 <ddeennyy@hotmail.it>
2020-04-06 18:05:27 +0200

    updated to POST request

--- xmasg/views.py
+++ xmasg/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.http import Http404, HttpResponse, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 import datetime
 from . import forms, models
@@ -13,7 +13,7 @@ def prepare_sidebar(current_user):
     '''
     sidebar_data = {}
     
-    sidebar_data['rooms'] = models.Room.objects.filter(end_date__gte=datetime.datetime.now()).all()   # Get available rooms
+    sidebar_data['rooms'] = models.Room.objects.filter(end_date__gte=datetime.datetime.now()).exclude(member=current_user).all()   # Get available rooms
     sidebar_data['myrooms'] = models.Room.objects.filter(member=current_user).all()   # ToDO: add filter on user subscribed
 
     return sidebar_data
@@ -50,19 +50,22 @@ def room(request, pk):
                                                 "form_add_member": form}) 
 
 @login_required
-def room_add_member(request, room_id, user_id):
+def room_add_member(request):
     '''
     Add a member to a room
     '''
     # Read form
-    if request.method == 'GET':
-        room = models.Room.objects.get(id=room_id)
-        user = User.objects.get(id=user_id)
-        room_members = room.member.all()
-        if user not in room_members:
-            room_member = models.RoomMember.objects.create(room=room, member=user, is_admin=False)
-            room_member.save()
+    if request.method == 'POST':
+        form = forms.RoomMemberForm(request.POST)
+        if form.is_valid():
+            room = models.Room.objects.get(id=form.cleaned_data['room_id'])
+            user = User.objects.get(id=form.cleaned_data['user_id'])
+            room_members = room.member.all()
+            if user not in room_members:
+                room_member = models.RoomMember.objects.create(room=room, member=user, is_admin=False)
+                room_member.save()
             return redirect('xmasg_room', pk=room.id)
+    return HttpResponseBadRequest('Wrong add member request')
 
 
 @login_required
@@ -80,23 +83,3 @@ def room_new(request):
     sidebar_data = prepare_sidebar(request.user)
     return render(request, 'xmasg/room_new.html', {"sidebar_data": sidebar_data,
                                                     "form": form}) 
-
-
-# # %% API (Ajax)
-# # @login_required
-# def room_add_member(request):
-#     '''
-#     Add a member to a room
-#     '''
-#     if request.is_ajax and request.method == "POST":
-#         form = forms.RoomMemberForm(request.POST)
-#         if form.is_valid():
-#             user1 = models.User.filter(id=form.user_id).first()
-#             room1 = models.Room.filter(id=form.room_id).first()
-#             room_member = models.RoomMember.objects.create(room=room1, 
-#                                                             member=user1, is_admin=False)
-#             room_member.save()
-#             return JsonResponse({"alert_message": f"{user1.username} added to room {room1.name}"}, status=200)
-#         else:
-#             return JsonResponse({"alert_message": form.errors}, status=400)
-#     return JsonResponse({"alert_message": "Wrong request"}, status=400)
