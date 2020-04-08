49e6577d8da30eb227e22349a8612afc2c254a98
Sawy89 <ddeennyy@hotmail.it>
2020-04-08 14:48:24 +0200

    button enabled only when something changes

--- xmasg/static/xmasg/room.js
+++ xmasg/static/xmasg/room.js
@@ -19,4 +19,37 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // Button modify members
+    document.querySelector('#bnt-change-member').disabled = true;
+    document.querySelectorAll('.room-member input').forEach(element => {
+        element.addEventListener("change", checkInputChange, false);
+    });
+
 });
+
+
+// If Input change from default, disable button
+function checkInputChange() {
+    buttonChange = document.querySelector('#bnt-change-member');
+    if (isEqualToDefault(this)) {
+        // if one is equal --> CHECK ALL to be sure!
+        buttonChange.disabled = true; // init = disable
+        document.querySelectorAll('.room-member input').forEach(element => {
+            if (isEqualToDefault(element) == false) {buttonChange.disabled = false;} // if one is different, enable
+        });
+    }
+    else {
+        // if one is different, enabled
+        buttonChange.disabled = false;
+    }
+};
+
+
+function isEqualToDefault(element) {
+    // Convert current status: "checked" or empty
+    if (element.checked) {var currentStatus = 'checked'}
+    else {var currentStatus = ''}
+    // Check if it's the same
+    if (element.dataset.defaultvalue == currentStatus) {return true;}
+    else {return false;}
+};
