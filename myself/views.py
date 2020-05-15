e0e6b694e0de3b41d70443e52817f52cd3f3d93b
Sawy89 <ddeennyy@hotmail.it>
2020-05-15 17:33:28 +0200

    new styling for myself page

--- myself/views.py
+++ myself/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'myself/index.html') 
+    request.is_free = True
+    testo = {}
+    testo['intro'] = "I'm a biomedical engineer, with some year of experience, interested in the biomedical field but also in the more general engineering. I'm a young guy searching the possibility to obtain new skills and new knowledge. I'v learned in my life hot to work in a team, obtaining the best results for me and my colleagues. In my first working experiences, I've been in very interesting and dynamic places, working with people from different country, improving my communicative skills and my technical knowledge. I've done many voluntary experience, and I like music, playing some instruments. "
+    print(testo['intro'])
+    return render(request, 'myself/index.html', {'testo': testo}) 
