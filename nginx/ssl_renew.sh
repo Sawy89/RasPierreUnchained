395761f2323b1046daf65c47e5ade51eb67098ae
Sawy89 <ddeennyy@hotmail.it>
2020-09-21 22:22:14 +0200

    fix folder name

--- nginx/ssl_renew.sh
+++ nginx/ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/sawy89/RaspierreUnchained
+cd /home/sawy89/RasPierreUnchained
 $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP ras_nginx
 $DOCKER system prune -af
