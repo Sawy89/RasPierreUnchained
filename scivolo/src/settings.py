c25d2c4308e2218b6f6a1185d1986ee1083c53ae
Sawy89 <ddeennyy@hotmail.it>
2022-05-02 22:50:46 +0200

    init scivolo app with fastapi

---
+++ scivolo/src/settings.py
@@ -0,0 +1,20 @@
+import os
+
+# SPECIFIC
+
+# CONSTANT
+LOG_CONF = os.environ.get("LOG_CONF", default="/usr/src/app/logging.conf") # need the env variable for vscode debug
+LOG_PATH = os.environ.get("LOG_PATH", default="/usr/data/app/log") # need the env variable for vscode debug
+
+# FROM ENV
+# DATABASE_IP = os.environ.get("DATABASE_IP", default="localhost:5432")
+# DATABASE_NAME = "scivolo"
+# DATABASE_USER = os.environ.get("DATABASE_USER", default="root")
+# DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", default="admin")
+
+# # Mail
+# EMAIL_HOST = os.environ.get("EMAIL_HOST", default="smtp.gmail.com")
+# EMAIL_PORT = os.environ.get("EMAIL_PORT", default="587")
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# EMAIL_ADMIN = os.environ.get("EMAIL_ADMIN")
--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
-docker-compose down
-docker-compose build
-docker-compose up
+@REM Batch file for launching the fastapi service inside docker with API for SII downloader
+if not exist "_backup" mkdir _backup
+if not exist "_backup\log" mkdir _backup\log
+
+@REM Start docker services
+docker-compose -f docker-compose.windev.yml down
+docker-compose -f docker-compose.windev.yml build
+docker-compose -f docker-compose.windev.yml up
