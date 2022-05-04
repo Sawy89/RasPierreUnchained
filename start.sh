809216bfdc1ce6cb912755beb42ed947456ef589
Sawy89 <ddeennyy@hotmail.it>
2022-05-04 11:18:51 +0200

    integrate new service backend (fastapi) in production

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+echo 'inizio sh'
+echo $(date)
+
+cd "$(dirname "$0")"
+
+# Create folder
+mkdir -p ./_backup
+mkdir -p ./_backup/log
+
 docker-compose -f docker-compose.prod.yml down -v
 docker-compose -f docker-compose.prod.yml up -d --build
+
+echo $(date)
+echo 'fine sh'
